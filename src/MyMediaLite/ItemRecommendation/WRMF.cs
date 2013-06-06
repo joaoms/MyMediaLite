2baec4b374773e90cc455d0ddc92bbde15aa39d4
João Vinagre <joaoms@joaoms.com>
2013-06-06 18:09:11 +0100

    Changed WRMF to cope nicely with batch_evaluator custom script

--- src/MyMediaLite/ItemRecommendation/WRMF.cs
+++ src/MyMediaLite/ItemRecommendation/WRMF.cs
@@ -18,6 +18,7 @@
 using System;
 using System.Globalization;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using MathNet.Numerics.LinearAlgebra.Double;
 using MyMediaLite.DataType;
 
@@ -63,6 +64,8 @@ namespace MyMediaLite.ItemRecommendation
 		public WRMF()
 		{
 			NumIter = 15;
+			//UpdateUsers = true;
+			//UpdateItems = true;
 		}
 
 		///
@@ -142,6 +145,111 @@ namespace MyMediaLite.ItemRecommendation
 			});
 		}
 
+		///
+		public override void AddFeedback(ICollection<Tuple<int, int>> feedback)
+		{
+			base.AddFeedback(feedback);
+			//FoldIn(feedback);
+		}
+		
+		///
+		public override void RemoveFeedback(ICollection<Tuple<int, int>> feedback)
+		{
+			base.RemoveFeedback(feedback);
+			// TODO
+			//FoldOut(feedback);
+		}
+
+		/*
+		void FoldIn(ICollection<Tuple<int, int>> feedback)
+		{
+			foreach (var entry in feedback)
+				UpdateFactors(entry.Item1, entry.Item2, UpdateUsers, UpdateItems);
+		}
+		*/
+		
+		///
+		protected override void AddUser(int user_id)
+		{
+			base.AddUser(user_id);
+			
+			user_factors.AddRows(user_id + 1);
+			user_factors.RowInitNormal(user_id, InitMean, InitStdDev);
+		}
+		
+		///
+		protected override void AddItem(int item_id)
+		{
+			base.AddItem(item_id);
+			
+			item_factors.AddRows(item_id + 1);
+			item_factors.RowInitNormal(item_id, InitMean, InitStdDev);
+		}
+		
+		
+		///
+		public override void RemoveUser(int user_id)
+		{
+			base.RemoveUser(user_id);
+			
+			// set user latent factors to zero
+			user_factors.SetRowToOneValue(user_id, 0);
+		}
+		
+		///
+		public override void RemoveItem(int item_id)
+		{
+			base.RemoveItem(item_id);
+			
+			// set item latent factors to zero
+			item_factors.SetRowToOneValue(item_id, 0);
+		}
+		
+		/* TODO
+		/// <summary>
+		/// Performs factor updates for a user and item pair.
+		/// </summary>
+		/// <param name="user_id">User_id.</param>
+		/// <param name="item_id">Item_id.</param>
+		protected virtual void UpdateFactors(int user_id, int item_id, bool update_user, bool update_item)
+		{
+			//Console.WriteLine(float.MinValue);
+			float err = 1 - Predict(user_id, item_id);
+			
+			// adjust factors
+			if(update_user)
+			{
+				// Get user's items
+				var user_items = Feedback.UserMatrix.GetEntriesByRow(user_id);
+				var if_t_if = new Matrix<float>(MaxItemID,MaxItemID);
+				for (int i_ft = 0; i_ft < num_factors; i_ft++)
+					for (int i_f = 0; i_f < num_factors; i_f++)
+					{
+						if_t_if[i_ft, i_f] = 0;
+						for (int i = 0; i <= MaxItemID; i++)
+							if_t_if[if_t, i_f] += item_factors[i, i_ft] * item_factors[i, i_f];
+					}
+
+				for (int f = 0; f < NumFactors; f++)
+				{
+					float u_f = user_factors[user_id, f];
+				
+				// if necessary, compute and apply updates
+				if (update_user)
+				{
+					// user_factors[user_id] += 
+					user_factors.Inc(user_id, f, current_learnrate * delta_u);
+				}
+				if (update_item)
+				{
+					double delta_i = err * u_f - Regularization * i_f;
+					item_factors.Inc(item_id, f, current_learnrate * delta_i);
+				}
+			}
+			
+		}
+		*/
+		
 		///
 		public override float ComputeObjective()
 		{
