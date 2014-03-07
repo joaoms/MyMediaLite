522b8e6a4c25ccbbeb7a22aa51e0b29fc49b86b9
João Vinagre <joaoms@joaoms.com>
2014-03-07 14:44:22 +0000

    NaiveSVD: changed recommendation code to better reflect the desired ordering

--- src/MyMediaLite/ItemRecommendation/NaiveSVD.cs
+++ src/MyMediaLite/ItemRecommendation/NaiveSVD.cs
@@ -223,72 +223,73 @@ namespace MyMediaLite.ItemRecommendation
 			if (n == -1)
 			{
 				var scored_items = new List<Tuple<int, float>>();
-
+				/*
 				foreach (int item_id in candidate_items)
 					if (!ignore_items.Contains(item_id))
 					{
-						float score = -Math.Abs(1 - Predict(user_id, item_id));
-						if (score > float.MinValue)
-							scored_items.Add(Tuple.Create(item_id, score));
+						float error = Math.Abs(1 - Predict(user_id, item_id));
+						if (error > float.MaxValue)
+							error = float.MaxValue;
+						scored_items.Add(Tuple.Create(item_id, error));
 					}
 
-				/*
+				*/
 				Parallel.ForEach(candidate_items, item_id => {
 					if (!ignore_items.Contains(item_id))
 					{
-						float score = -Math.Abs(1 - Predict(user_id, item_id));
-						if (score > float.MinValue)
-							lock(scored_items)
-								scored_items.Add(Tuple.Create(item_id, score));
+						float error = Math.Abs(1 - Predict(user_id, item_id));
+						if (error > float.MaxValue)
+							error = float.MaxValue;
+						lock(scored_items)
+							scored_items.Add(Tuple.Create(item_id, score));
 					}
 				});
-				*/
-				ordered_items = scored_items.OrderByDescending(x => x.Item2).ToArray();
+
+				ordered_items = scored_items.OrderBy(x => x.Item2).ToArray();
 			}
 			else
 			{
 				var comparer = new DelegateComparer<Tuple<int, float>>( (a, b) => a.Item2.CompareTo(b.Item2) );
 				var heap = new IntervalHeap<Tuple<int, float>>(n, comparer);
-				float min_relevant_score = float.MinValue;
-
+				float max_error = float.MaxValue;
+				/*
 				foreach (int item_id in candidate_items)
 					if (!ignore_items.Contains(item_id))
 					{
-						float score = -Math.Abs(1 - Predict(user_id, item_id));
-						if (score > min_relevant_score)
+						float error = Math.Abs(1 - Predict(user_id, item_id));
+						if (error < max_error)
 						{
-							heap.Add(Tuple.Create(item_id, score));
+							heap.Add(Tuple.Create(item_id, error));
 							if (heap.Count > n)
 							{
-								heap.DeleteMin();
-								min_relevant_score = heap.FindMin().Item2;
+								heap.DeleteMax();
+								max_error = heap.FindMax().Item2;
 							}
 						}
 					}
-
-				/*
+				*/
 				Parallel.ForEach(candidate_items, item_id => {
 					if (!ignore_items.Contains(item_id))
 					{
-						float score = -Math.Abs(1 - Predict(user_id, item_id));
-						if (score > min_relevant_score)
+						float error = Math.Abs(1 - Predict(user_id, item_id));
+						if (error < max_error)
 						{
 							lock (heap)
 							{
-								heap.Add(Tuple.Create(item_id, score));
+								heap.Add(Tuple.Create(item_id, error));
 								if (heap.Count > n)
 								{
-									heap.DeleteMin();
-									min_relevant_score = heap.FindMin().Item2;
+									heap.DeleteMax();
+									max_error = heap.FindMax().Item2;
 								}
 							}
 						}
 					}
 				});
-				*/
+
 				ordered_items = new Tuple<int, float>[heap.Count];
 				for (int i = 0; i < ordered_items.Count; i++)
-					ordered_items[i] = heap.DeleteMax();
+					ordered_items[i] = heap.DeleteMin();
 			}
 
 			return ordered_items;
