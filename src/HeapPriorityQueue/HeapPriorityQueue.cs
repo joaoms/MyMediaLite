c87eb8890b39389a1fbefb0a7f57d40ddb8dbaec
João Vinagre <joaoms@joaoms.com>
2014-10-28 17:41:32 +0000

    Completely changed implementation

---
+++ src/HeapPriorityQueue/HeapPriorityQueue.cs
@@ -0,0 +1,269 @@
+﻿//
+//  Copyright 2012  Patrick Uhlmann
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+using System;
+using System.Collections.Generic;
+
+namespace Uhlme
+{
+	/// <summary>
+	/// Heap priority queue. Uses a Dictionary to speed up the UpdatePriority method. It is only allowed to add each element once
+	/// Some operations are implemented using a Dictionary. In order for them to run fast T should have a good hash
+	/// </summary>
+	public class HeapPriorityQueue<T>
+	{
+		private static int DEFAULTCAPACITY = 1024;
+
+		private KeyValueEntry<double, T>[] queue;
+
+		private Dictionary<T, int> entryToPos;
+
+		private int count;
+
+		public HeapPriorityQueue () : this(DEFAULTCAPACITY)
+		{
+		}
+
+		public HeapPriorityQueue (int capacity)
+		{
+			queue = new KeyValueEntry<double, T>[capacity];
+			entryToPos = new Dictionary<T, int>(capacity);
+		}
+
+		public int Count {
+			get {
+				return count;
+			}
+		}
+
+		public Boolean IsEmpty ()
+		{
+			return Count == 0;
+		}
+
+		/// <summary>
+		/// Operation: O(1)
+		/// </summary>
+		public void Clear ()
+		{
+			count = 0;
+			queue = new KeyValueEntry<double, T>[DEFAULTCAPACITY];
+			entryToPos.Clear();
+		}
+
+		/// <summary>
+		/// Operation: O(1)
+		/// </summary>
+		public bool Contains(T element) {
+			return entryToPos.ContainsKey (element);
+		}
+
+		/// <summary>
+		/// Operation: O(1)
+		/// </summary>
+		public double PeekPriority ()
+		{
+			if (IsEmpty()) {
+				return 0;
+			}
+
+			return queue[0].Key;
+		}
+
+		/// <summary>
+		/// Operation: O(1)
+		/// </summary>
+		public T Peek ()
+		{
+			if (IsEmpty()) {
+				return default(T);
+			}
+
+			return queue[0].Value;
+		}
+
+		/// <summary>
+		/// If the queue is empty we do just return null
+		/// 
+		/// Operation: O(log n)
+		/// </summary>
+		public T Dequeue ()
+		{
+			if (IsEmpty()) {
+				return default(T);
+			}
+
+			KeyValueEntry<double, T> result = queue[0];
+
+			queue[0] = queue[count-1];
+			RemovePosition(result.Value);
+			UpdatePosition(queue[0].Value, 0);
+			queue[count-1] = null;
+			count --;
+			BubbleDown(0);
+
+			return result.Value;
+		}
+
+		/// <summary>
+		/// Operation: O(log n)
+		/// 
+		/// InvalidOperationException: If we add an element which is alread in the Queue
+		/// </summary>
+		public void Enqueue (T element, double priority)
+		{
+			if (Contains (element)) {
+				throw new InvalidOperationException("Cannot add an element which is already in the queue");
+			}
+
+			if (count == queue.Length) {
+				KeyValueEntry<double, T>[] oldQueue = queue;
+				queue = new KeyValueEntry<double, T>[oldQueue.Length * 3 / 2 + 1];
+				Array.Copy(oldQueue, queue, oldQueue.Length);
+			}
+
+			queue[count] = new KeyValueEntry<double, T>(priority, element);
+			UpdatePosition(element, count);
+			count++;
+
+			BubbleUp(count-1);
+		}
+
+		/// <summary>
+		/// Operation: O(log n)
+		/// 
+		/// InvalidOperationException: If we want to update the priority of an element which is not in the queue
+		/// </summary>
+		public void UpdatePriority (T element, double newPrio)
+		{
+			if (!entryToPos.ContainsKey (element)) {
+				throw new InvalidOperationException ("Cannot update the priority of an element which is not in the queue");
+			}
+
+			int pos = entryToPos [element];
+			double oldPrio = queue [pos].Key;
+
+			if (oldPrio == newPrio) {
+				return;
+			}
+
+			queue [pos].Key = newPrio;
+
+			if (oldPrio < newPrio) {
+				BubbleDown(pos);
+			} else {
+				BubbleUp(pos);
+			}
+		}
+
+		/// <summary>
+		/// Moving root to correct location
+		/// </summary>
+		private void BubbleDown (int i)
+		{
+			int minPos;
+			KeyValueEntry<double, T> min;
+			int left;
+			int right;
+
+			while (true) {
+				minPos = i;
+				min = queue[i];
+				left = 2 * i + 1;
+				right = 2 * i + 2;
+
+				if (left < count && queue[left].Key < min.Key) {
+					minPos = left;
+					min = queue[left];
+				}
+
+				if (right < count && queue[right].Key < min.Key) {
+					minPos = right;
+					min = queue[right];
+				}
+
+				if (min == queue[i]) {
+					break;
+				} else {
+					// swap
+					queue[minPos] = queue[i];
+					queue[i] = min;
+
+					UpdatePosition(queue[minPos].Value, minPos);
+					UpdatePosition(queue[i].Value, i);
+
+					i = minPos;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Moving last element of last level to correct location
+		/// </summary>
+		private void BubbleUp (int i)
+		{
+			int n = i;
+			int up = (n - 1) / 2;
+
+			while (up >= 0 && queue[up].Key > queue[n].Key) {
+				var entry = queue[up];
+				queue[up] = queue[n];
+				queue[n] = entry;
+
+				UpdatePosition(queue[n].Value, n);
+				UpdatePosition(queue[up].Value, up);
+
+				n = up;
+				up = (up-1)/2;
+			}
+		}
+
+		private void UpdatePosition (T element, int pos)
+		{
+			if (entryToPos.ContainsKey (element)) {
+				entryToPos.Remove (element);
+			}
+
+			entryToPos.Add(element, pos);
+		}
+
+		private void RemovePosition (T element)
+		{
+			entryToPos.Remove(element);
+		}
+
+		public void ScaleKeys(double alpha)
+		{
+			if (alpha <= 0) return;
+
+			for (int i = 0; i < count; i++)
+				queue[i].Key *= (1 - alpha);
+
+		}
+
+		public void AddOrUpdate(T element, double alpha)
+		{
+			ScaleKeys(alpha);
+			if (entryToPos.ContainsKey(element))
+			{
+				double oldPrio = queue[entryToPos[element]].Key;
+				double newPrio = ((1 - alpha)*oldPrio) + alpha;
+				UpdatePriority(element, newPrio);
+			}
+			else
+				Enqueue(element, alpha);
+		}
+
+	}
+}
--- src/MyMediaLite/ItemRecommendation/SimpleSGDPoisson.cs
+++ src/MyMediaLite/ItemRecommendation/SimpleSGDPoisson.cs
@@ -24,6 +24,7 @@ using System.Threading.Tasks;
 using MathNet.Numerics.LinearAlgebra.Double;
 using MyMediaLite.DataType;
 using MyMediaLite.Data;
+using Uhlme; 
 
 namespace MyMediaLite.ItemRecommendation
 {
@@ -42,8 +43,8 @@ namespace MyMediaLite.ItemRecommendation
 		private bool forget_users;
 		private bool forget_items;
 
-		BinaryHeap item_queue;
-		BinaryHeap user_queue;
+		HeapPriorityQueue<int> item_queue;
+		HeapPriorityQueue<int> user_queue;
 
 		// float max_score = 1.0f;
 
@@ -65,45 +66,62 @@ namespace MyMediaLite.ItemRecommendation
 
 			if (forget_users)
 			{
-				user_queue = new BinaryHeap();
+				user_queue = new HeapPriorityQueue<int>(Feedback.Users.Count);
 				foreach (int usr in Feedback.Users)
 					user_queue.AddOrUpdate(usr, Alpha);
 			}
 			if (forget_items)
 			{
-				item_queue = new BinaryHeap();
+				item_queue = new HeapPriorityQueue<int>(Feedback.Items.Count);
 				foreach (int item in Feedback.Items)
+				{
+					Console.WriteLine("Adding item "+item);
 					item_queue.AddOrUpdate(item, Alpha);
+				}
+				//DEBUG stuff
+				int cnt;
+				Console.WriteLine("Heap has "+ (cnt = item_queue.Count) +" elements");
+
+				for (int i = 0; i < cnt; i++)
+					Console.WriteLine("Prio: "+item_queue.PeekPriority()+" item "+item_queue.Dequeue());
 			}
 		}
 
 		protected override void Retrain(System.Collections.Generic.ICollection<Tuple<int, int>> feedback)
 		{
-			BHNode[] qu = new BHNode[Math.Min(ForgetHorizon, user_queue.Count - 1)];
-			BHNode[] qi = new BHNode[Math.Min(ForgetHorizon, item_queue.Count - 1)];
+			int[] qu = new int[Math.Min(ForgetHorizon, user_queue.Count - 1)];
+			double[] qu_prio = new double[Math.Min(ForgetHorizon, user_queue.Count - 1)];
+			int[] qi = new int[Math.Min(ForgetHorizon, item_queue.Count - 1)];
+			double[] qi_prio = new double[Math.Min(ForgetHorizon, item_queue.Count - 1)];
 			foreach (var entry in feedback)
 			{
 				if (forget_users)
 				{
 					for (uint i = 0; i < qu.Length; i++)
-						qu[i] = user_queue.Remove();
+					{
+						qu_prio[i] = user_queue.PeekPriority();
+						qu[i] = user_queue.Dequeue();
+					}
 				}
 				if (forget_items)
 				{
 					for (uint i = 0; i < qi.Length; i++)
-						qi[i] = item_queue.Remove();
+					{
+						qi_prio[i] = item_queue.PeekPriority();
+						qi[i] = item_queue.Dequeue();
+					}
 				}
 				//Console.WriteLine("Forgetting item "+qi);
 				if (forget_users)
 					foreach (var usr in qu.Reverse())
-						if (usr.Idx >= 0 && usr.Idx != entry.Item1) 
+						if (usr >= 0 && usr != entry.Item1) 
 							for (uint i = 0; i < IncrIter; i++)
-								UpdateFactors(usr.Idx, entry.Item2, ForgetUsersInUsers, ForgetUsersInItems, 0);
+								UpdateFactors(usr, entry.Item2, ForgetUsersInUsers, ForgetUsersInItems, 0);
 				if (forget_items)
 					foreach (var itm in qi.Reverse())
-						if (itm.Idx >= 0 && itm.Idx != entry.Item2)
+						if (itm >= 0 && itm != entry.Item2)
 							for (uint i = 0; i < IncrIter; i++)
-								UpdateFactors(entry.Item1, itm.Idx, ForgetItemsInUsers, ForgetItemsInItems, 0);
+								UpdateFactors(entry.Item1, itm, ForgetItemsInUsers, ForgetItemsInItems, 0);
 				for (uint i = 0; i < IncrIter; i++)
 					UpdateFactors(entry.Item1, entry.Item2, UpdateUsers, UpdateItems, 1);
 
@@ -111,22 +129,22 @@ namespace MyMediaLite.ItemRecommendation
 				{
 					item_queue.AddOrUpdate(entry.Item2, Alpha);
 
-					foreach (var itm in qi.Reverse())
-						if (itm.Idx != entry.Item2)
+					for (int i = qi.Length - 1; i >= 0; i--)
+						if (qi[i] != entry.Item2)
 						{
-							itm.Lambda = alpha + ((1-alpha)*itm.Lambda);
-							item_queue.Add(itm);
+							var lambda = alpha + ((1-alpha)*qi_prio[i]);
+							item_queue.Enqueue(qi[i], lambda);
 						}
 				}
 				if (forget_users)
 				{
 					user_queue.AddOrUpdate(entry.Item1, Alpha);
 
-					foreach (var usr in qu.Reverse())
-						if (usr.Idx != entry.Item1)
+					for (int i = qu.Length - 1; i >= 0; i--)
+						if (qu[i] != entry.Item1)
 						{
-							usr.Lambda = alpha + ((1-alpha)*usr.Lambda);
-							user_queue.Add(usr);
+							var lambda = alpha + ((1-alpha)*qu_prio[i]);
+							user_queue.Enqueue(qu[i], lambda);
 						}
 				}
 			}
@@ -138,7 +156,10 @@ namespace MyMediaLite.ItemRecommendation
 			base.RemoveItem(item_id);
 
 			if (forget_items)
-				item_queue.Remove(item_id);
+			{
+				// TODO: Implement queue remove
+				//item_queue.Remove(item_id);
+			}
 		}
 
 		///
@@ -147,7 +168,10 @@ namespace MyMediaLite.ItemRecommendation
 			base.RemoveUser(user_id);
 
 			if (forget_users)
-				user_queue.Remove(user_id);
+			{
+				// TODO: Implement queue remove
+				//user_queue.Remove(user_id);
+			}
 		}
 
 		/// <summary>
@@ -187,309 +211,12 @@ namespace MyMediaLite.ItemRecommendation
 		{
 			return string.Format(
 				CultureInfo.InvariantCulture,
-				"NaiveSVDZI num_factors={0} regularization={1} learn_rate={2} num_iter={3} incr_iter={4} decay={5}, forget_horizon={6}",
-				NumFactors, Regularization, LearnRate, NumIter, IncrIter, Decay, ForgetHorizon);
+				"SimpleSGDPoisson num_factors={0} regularization={1} learn_rate={2} num_iter={3} incr_iter={4} decay={5}, forget_horizon={6}, alpha={7}",
+				NumFactors, Regularization, LearnRate, NumIter, IncrIter, Decay, ForgetHorizon, Alpha);
 		}
 
 
 	}
 
-	class BHNode : IComparable<BHNode>
-	{
-		private int idx;
-		private double priority;
-		public int Idx { get { return idx; } set { idx = value; } }
-		public double Lambda { get { return priority; } set { priority = value; } }
-		
-		public BHNode(int in_idx, double lambda)
-		{
-			Idx = in_idx;
-			Lambda = lambda;
-		}
-
-		public BHNode(int in_idx)
-		{
-			Idx = in_idx;
-			Lambda = 0;
-		}
-
-		public int CompareTo(BHNode other)
-		{
-			return this.Idx - other.Idx;
-		}
-
-	}
-
-	/// <summary>
-	/// A binary heap, useful for sorting data and priority queues.
-	/// </summary>
-	class BinaryHeap : IEnumerable<BHNode>
-	{
-		// Constants
-		private const int DEFAULT_SIZE = 1000;
-		// Fields
-		private BHNode[] _data = new BHNode[DEFAULT_SIZE];
-		private int _count = 0;
-		private int _capacity = DEFAULT_SIZE;
-		private bool _sorted;
-
-		// Properties
-		/// <summary>
-		/// Gets the number of values in the heap. 
-		/// </summary>
-		public int Count
-		{
-			get { return _count; }
-		}
-		/// <summary>
-		/// Gets or sets the capacity of the heap.
-		/// </summary>
-		public int Capacity
-		{
-			get { return _capacity; }
-			set
-			{
-				int previousCapacity = _capacity;
-				_capacity = Math.Max(value, _count);
-				if (_capacity != previousCapacity)
-				{
-					BHNode[] temp = new BHNode[_capacity];
-					Array.Copy(_data, temp, _count);
-					_data = temp;
-				}
-			}
-		}
-		// Methods
-		/// <summary>
-		/// Creates a new binary heap.
-		/// </summary>
-		public BinaryHeap()
-		{
-		}
-		private BinaryHeap(BHNode[] data, int count)
-		{
-			Capacity = count;
-			_count = count;
-			Array.Copy(data, _data, count);
-		}
-		/// <summary>
-		/// Gets the first value in the heap without removing it.
-		/// </summary>
-		/// <returns>The lowest value of type TValue.</returns>
-		public BHNode Peek()
-		{
-			return _data[0];
-		}
-
-		/// <summary>
-		/// Removes all items from the heap.
-		/// </summary>
-		public void Clear()
-		{
-			this._count = 0;
-			_data = new BHNode[_capacity];
-		}
-		/// <summary>
-		/// Adds a key and value to the heap.
-		/// </summary>
-		/// <param name="item">The item to add to the heap.</param>
-		public void Add(BHNode item)
-		{
-			if (_count == _capacity)
-			{
-				Capacity *= 2;
-			}
-			_data[_count] = item;
-			UpHeap();
-			_count++;
-		}
-
-		public void AddOrUpdate(int idx, float alpha)
-		{
-			double z0 = 0;
-			var removed = Remove(idx);
-			if (removed != null)
-				z0 = removed.Lambda;
-			Scale(alpha);
-			Add(new BHNode(idx, ((1-alpha)*z0) + alpha));
-		}
-			
-		/// <summary>
-		/// Removes and returns the first item in the heap.
-		/// </summary>
-		/// <returns>The next value in the heap.</returns>
-		public BHNode Remove()
-		{
-			if (this._count == 0)
-			{
-				throw new InvalidOperationException("Cannot remove item, heap is empty.");
-			}
-			BHNode v = _data[0];
-			_count--;
-			_data[0] = _data[_count];
-			_data[_count] = null; //Clears the Last Node
-			DownHeap();
-			return v;
-		}
-		private void UpHeap()
-		//helper function that performs up-heap bubbling
-		{
-			_sorted = false;
-			int p = _count;
-			BHNode item = _data[p];
-			int par = Parent(p);
-			while (par > -1 && item.Lambda < _data[par].Lambda)
-			{
-				_data[p] = _data[par]; //Swap nodes
-				p = par;
-				par = Parent(p);
-			}
-			_data[p] = item;
-		}
-		private void DownHeap()
-		//helper function that performs down-heap bubbling
-		{
-			_sorted = false;
-			int n;
-			int p = 0;
-			BHNode item = _data[p];
-			while (true)
-			{
-				int ch1 = Child1(p);
-				if (ch1 >= _count) break;
-				int ch2 = Child2(p);
-				if (ch2 >= _count)
-				{
-					n = ch1;
-				}
-				else
-				{
-					n = _data[ch1].Lambda < _data[ch2].Lambda ? ch1 : ch2;
-				}
-				if (item.Lambda > _data[n].Lambda)
-				{
-					_data[p] = _data[n]; //Swap nodes
-					p = n;
-				}
-				else
-				{
-					break;
-				}
-			}
-			_data[p] = item;
-		}
-		private void EnsureSort()
-		{
-			if (_sorted) return;
-			Array.Sort(_data, 0, _count);
-			_sorted = true;
-		}
-		private static int Parent(int index)
-		//helper function that calculates the parent of a node
-		{
-			return (index - 1) >> 1;
-		}
-		private static int Child1(int index)
-		//helper function that calculates the first child of a node
-		{
-			return (index << 1) + 1;
-		}
-		private static int Child2(int index)
-		//helper function that calculates the second child of a node
-		{
-			return (index << 1) + 2;
-		}
-
-		/// <summary>
-		/// Creates a new instance of an identical binary heap.
-		/// </summary>
-		/// <returns>A BinaryHeap.</returns>
-		public BinaryHeap Copy()
-		{
-			return new BinaryHeap(_data, _count);
-		}
-
-		/// <summary>
-		/// Gets an enumerator for the binary heap.
-		/// </summary>
-		/// <returns>An IEnumerator of type T.</returns>
-		public IEnumerator<BHNode> GetEnumerator()
-		{
-			EnsureSort();
-			for (int i = 0; i < _count; i++)
-			{
-				yield return _data[i];
-			}
-		}
-		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
-		}
-
-		/// <summary>
-		/// Checks to see if the binary heap contains the specified item.
-		/// </summary>
-		/// <param name="item">The item to search the binary heap for.</param>
-		/// <returns>A boolean, true if binary heap contains item.</returns>
-		public bool Contains(BHNode item)
-		{
-			EnsureSort();
-			return Array.BinarySearch<BHNode>(_data, 0, _count, item) >= 0;
-		}
-
-		public bool Contains(int idx)
-		{
-			return Contains(new BHNode(idx));
-		}
-
-		/// <summary>
-		/// Copies the binary heap to an array at the specified index.
-		/// </summary>
-		/// <param name="array">One dimensional array that is the destination of the copied elements.</param>
-		/// <param name="arrayIndex">The zero-based index at which copying begins.</param>
-		public void CopyTo(BHNode[] array, int arrayIndex)
-		{
-			EnsureSort();
-			Array.Copy(_data, array, _count);
-		}
-		/// <summary>
-		/// Gets whether or not the binary heap is readonly.
-		/// </summary>
-		public bool IsReadOnly
-		{
-			get { return false; }
-		}
-		/// <summary>
-		/// Removes an item from the binary heap. This utilizes the type T's Comparer and will not remove duplicates.
-		/// </summary>
-		/// <param name="item">The item to be removed.</param>
-		/// <returns>The removed item or null.</returns>
-		public BHNode Remove(BHNode item)
-		{
-			EnsureSort();
-			int i = Array.BinarySearch<BHNode>(_data, 0, _count, item);
-			if (i < 0) return null;
-			BHNode removed = _data[i];
-			Array.Copy(_data, i + 1, _data, i, _count - i);
-			_data[_count] = null;
-			_count--;
-			return removed;
-		}
-
-		public BHNode Remove(int idx)
-		{
-			return Remove(new BHNode(idx));
-		}
-
-		public void Scale(float alpha)
-		{
-			if (alpha <= 0) throw new Exception("only order-preserving scaling is allowed");
-			for (int i = 0; i < _count; i++)
-			{
-				_data[i].Lambda *= (1 - alpha);
-			}
-		}
-
-	}
 }
 
