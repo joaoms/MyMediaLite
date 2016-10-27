0e7f217e2e965eacd635374aaf52f73782e6e3f1
João Vinagre <joaoms@joaoms.com>
2016-10-27 10:46:40 +0100

    Re-enable time measurements

--- src/Programs/KFoldPrequentialEval/KFoldPrequentialEval.cs
+++ src/Programs/KFoldPrequentialEval/KFoldPrequentialEval.cs
@@ -219,29 +219,29 @@ class KFoldPrequentialEval
 			List<int> rec_list,til;
 			ISet<int> ignore_items;
 			Tuple<int,int> tuple;
-			//DateTime ts;
-			//double[] tsums = Enumerable.Repeat(0D,14).ToArray();
+			DateTime ts;
+			double[] tsums = Enumerable.Repeat(0D,14).ToArray();
 
 			for (int i = 0; i < fold_test_data[f].Count; i++)
 			{
-				//ts = DateTime.Now;
+				ts = DateTime.Now;
 				int user = fold_test_data[f].Users[i];
 				int item = fold_test_data[f].Items[i];
-				//tsums[0] += (DateTime.Now - ts).TotalMilliseconds;
-				//ts = DateTime.Now;
+				tsums[0] += (DateTime.Now - ts).TotalMilliseconds;
+				ts = DateTime.Now;
 
 				recommend = false;
 				if (fold_train_data[f].Users.Contains(user))
 				{
-					//tsums[13] += (DateTime.Now - ts).TotalMilliseconds;
+					tsums[13] += (DateTime.Now - ts).TotalMilliseconds;
 					recommend = true;
 					if (!repeated_items) {
 					   	int tmp;
 						recommend &= !fold_train_data[f].TryGetIndex(user, item, out tmp);
 					}
 				}
-				//tsums[1] += (DateTime.Now - ts).TotalMilliseconds;
-				//ts = DateTime.Now;
+				tsums[1] += (DateTime.Now - ts).TotalMilliseconds;
+				ts = DateTime.Now;
 		
 				if(recommend)
 				{
@@ -249,47 +249,47 @@ class KFoldPrequentialEval
 						ignore_items = new HashSet<int>();
 					else
 						ignore_items = new HashSet<int>(fold_train_data[f].UserMatrix[user]);
-					//tsums[2] += (DateTime.Now - ts).TotalMilliseconds;
-					//ts = DateTime.Now;
+					tsums[2] += (DateTime.Now - ts).TotalMilliseconds;
+					ts = DateTime.Now;
 
 					var rec_start = DateTime.Now;
 					rec_list_score = recommenders[f].Recommend(user, n_recs, ignore_items, candidate_items);
 					var rec_end = DateTime.Now;
-					//tsums[3] += (rec_end - rec_start).TotalMilliseconds;
-					//ts = DateTime.Now;
+					tsums[3] += (rec_end - rec_start).TotalMilliseconds;
+					ts = DateTime.Now;
 
 					rec_list = new List<int>();
 					foreach (var rec in rec_list_score)
 						rec_list.Add(rec.Item1);
 					
 					til = new List<int>(){ item };
-					//tsums[4] += (DateTime.Now - ts).TotalMilliseconds;
-					//ts = DateTime.Now;					
+					tsums[4] += (DateTime.Now - ts).TotalMilliseconds;
+					ts = DateTime.Now;					
 
 					results["recall_1"][f].Add(PrecisionAndRecall.RecallAt(rec_list, til, 1));
-					//tsums[5] += (DateTime.Now - ts).TotalMilliseconds;
-					//ts = DateTime.Now;
+					tsums[5] += (DateTime.Now - ts).TotalMilliseconds;
+					ts = DateTime.Now;
 
 					results["recall_5"][f].Add(PrecisionAndRecall.RecallAt(rec_list, til, 5));
 					results["recall_10"][f].Add(PrecisionAndRecall.RecallAt(rec_list, til, 10));
 					results["recall_20"][f].Add(PrecisionAndRecall.RecallAt(rec_list, til, 20));
-					//tsums[6] += (DateTime.Now - ts).TotalMilliseconds;
-					//ts = DateTime.Now;
+					tsums[6] += (DateTime.Now - ts).TotalMilliseconds;
+					ts = DateTime.Now;
 
 					results["map"][f].Add(PrecisionAndRecall.AP(rec_list, til));
 					var num_dropped = candidate_items.Count - ignore_items.Count - n_recs;
 					results["auc"][f].Add(AUC.Compute(rec_list, til, num_dropped));
 					results["ndcg"][f].Add(NDCG.Compute(rec_list, til));
 					results["rec_time"][f].Add((rec_end - rec_start).TotalMilliseconds);
-					//tsums[7] += (DateTime.Now - ts).TotalMilliseconds;
-					//ts = DateTime.Now;
+					tsums[7] += (DateTime.Now - ts).TotalMilliseconds;
+					ts = DateTime.Now;
 				
 					output_buffer[f, output_buffer_count[f]] = i + "\t" + user_mapping.ToOriginalID(user) + "\t" + item_mapping.ToOriginalID(item);
 					for (int k = 0; k < metrics.Count(); k++)
 						output_buffer[f, output_buffer_count[f]] += "\t" + results[metrics[k]][f].Last();
 					output_buffer_count[f]++;
-					//tsums[8] += (DateTime.Now - ts).TotalMilliseconds;
-					//ts = DateTime.Now;
+					tsums[8] += (DateTime.Now - ts).TotalMilliseconds;
+					ts = DateTime.Now;
 				}
 
 				// update recommenders
@@ -300,30 +300,30 @@ class KFoldPrequentialEval
 
 				train_end = DateTime.Now;
 				results["upd_time"][f].Add((train_end - train_start).TotalMilliseconds);
-				//tsums[9] += results["upd_time"][f].Last();
-				//ts = DateTime.Now;
+				tsums[9] += results["upd_time"][f].Last();
+				ts = DateTime.Now;
 
 				output_buffer_time[f, output_buffer_count_time[f]] = i + "\t" + user_mapping.ToOriginalID(user) + "\t" 
 				                                                                            + item_mapping.ToOriginalID(item);
 				output_buffer_time[f, output_buffer_count_time[f]] += "\t" + results["upd_time"][f].Last();
 				output_buffer_count_time[f]++;
-				//tsums[10] += (DateTime.Now - ts).TotalMilliseconds;
-				//ts = DateTime.Now;
+				tsums[10] += (DateTime.Now - ts).TotalMilliseconds;
+				ts = DateTime.Now;
 
 				WriteOutputBuffer(f);
-				//tsums[11] += (DateTime.Now - ts).TotalMilliseconds;
-				//ts = DateTime.Now;
+				tsums[11] += (DateTime.Now - ts).TotalMilliseconds;
+				ts = DateTime.Now;
 
 				if(i % 5000 == 0)
 					GC.Collect();
-				//tsums[12] += (DateTime.Now - ts).TotalMilliseconds;
-				/*ts = DateTime.Now;
+				tsums[12] += (DateTime.Now - ts).TotalMilliseconds;
+				ts = DateTime.Now;
 				if(i % 1000 == 0){
 				     Console.Write("Fold " + f + ":\t");
 				     for(int k = 0; k < tsums.Length; k++)
 				     	     Console.Write(Math.Round(tsums[k],0) + "\t");
 				     Console.WriteLine();
-				}*/
+				}
 			}
 			WriteOutputBuffer(f,true);
 		});
